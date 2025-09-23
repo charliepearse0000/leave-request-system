d4387cb363a3d0f30952f4f21a0c06be02bd6437
charlie <cpgh4321@gmail.com>
2025-09-23 10:18:35 +0200

    Fix syntax error in requests page - remove stray closing bracket

--- src/app/requests/page.tsx
+++ src/app/requests/page.tsx
@@ -349,7 +349,6 @@ const LeaveRequestsPage = () => {
             </div>
           </div>
 
-)}
           {viewIndicator && (
             <div className="mb-6">
               <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
