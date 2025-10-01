f82a6013457ae09471ac6b0150eb3d256976a307
charlie <cpgh4321@gmail.com>
2025-10-01 23:38:05 +0100

    chore(api): remove legacy 'DONT EDIT ANYTHING IN HERE (API)' folder after rename to 'API'

--- DONT EDIT ANYTHING IN HERE (API)/tests/global-setup.ts
+++
@@ -1,5 +0,0 @@
-
-export default async function globalSetup() {
-  process.env.NODE_ENV = "test";
-  process.env.JWT_SECRET = "test-secret";
-}
--- DONT EDIT ANYTHING IN HERE (API)/tests/global-teardown.ts
+++
@@ -1,6 +0,0 @@
-import { db } from '../src/config/database';
-export default async function globalTeardown() {
-  if (db.getDataSource().isInitialized) {
-    await db.getDataSource().destroy();
-  }
-} 
